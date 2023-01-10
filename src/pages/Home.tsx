d32867089f1507c5fc93f56797acca46383a8e4c
cyrus311 <shadow15mr@hotmail.com>
2023-01-10 14:07:36 +0300

    load more button added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { IHomePageProps } from "../interfaces/IHomePage";
 import { useAppSelector, useAppDispatch } from "../utils/hooks";
 import { getCharacters } from "../store/reducers/characterSlice";
 import TitlebarImageList from "../components/Characters/TitlebarImageList";
+import { Button } from "@mui/material";
+import Loader from "../components/UI/Loader/Loader";
 
 const Home = (props: IHomePageProps) => {
-  const chars = useAppSelector((state) => state.character);
+  const [page, setPage] = useState(1);
+  const [charList, setCharList] = useState([]);
+  const { characters, isLoading } = useAppSelector((state) => state.character);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(getCharacters(1));
-  }, [dispatch]);
+    dispatch(getCharacters(page));
+    if (characters) {
+      setCharList((prevState) => [
+        ...new Set(prevState.concat(characters.results as any))
+      ]);
+    }
+  }, [dispatch, page, characters]);
 
-  return chars.characters ? (
-    <TitlebarImageList data={chars.characters.results} />
-  ) : null;
+  return (
+    <>
+      {charList ? <TitlebarImageList data={charList} /> : null}
+      {isLoading ? <Loader /> : null}
+      {characters?.info?.next !== null ? (
+        <Button
+          variant="text"
+          onClick={() => setPage((prevState) => prevState + 1)}
+          style={{ marginBottom: "10px" }}
+        >
+          Load more...
+        </Button>
+      ) : null}
+    </>
+  );
 };
 
 export default Home;
