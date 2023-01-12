5efc30fe68ca30ac1b3739fb80dd2416aedf6756
cyrus311 <shadow15mr@hotmail.com>
2023-01-12 23:34:46 +0300

    header css updated

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
 import styles from "./Header.module.css";
 import { IHeaderProps } from "./types";
 
 const Header = (props: IHeaderProps) => {
   return (
     <header className={`${styles.mainHeader} ${props.cssClass}`}>
-      {props.title}
+      <Link to="/">
+        <HomeIcon /> Home
+      </Link>
+      <div>{props.title}</div>
     </header>
   );
 };
